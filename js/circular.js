d803cdb573727c8dee47baec4610a6f003b466d9
mandeepb29 <mandeepb29@gmail.com>
2019-09-01 20:31:28 +0530

    size reduced

--- js/circular.js
+++ js/circular.js
@@ -1,158 +1,143 @@
 //**----------------------------canvas with circular disc animation-----------------------***** /
 
-    // Initial Setup
-    const canvas = document.querySelector('canvas');
-    const c = canvas.getContext('2d');
-    
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+// Initial Setup
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-   
-    
-      const mouse = {
-        x:innerWidth/5,
-        y:innerHeight/2.5 
-       };
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-       if(innerWidth<=768){
-        mouse.x=innerWidth/3.5,
-        mouse.y=innerHeight/3.2
-   }
+const mouse = {
+  x: innerWidth / 5,
+  y: innerHeight / 2.5
+};
 
-       if(innerWidth<=480){
-        mouse.x=innerWidth/2,
-        mouse.y=innerHeight/1.65
-   }
-    
-    
-    // Variables
-  
-    //color of the circle
-    const colors = [
-    'grey',
-    '266da7',
-    'slategrey'];
-    
-    
-    
-    // Event Listeners
-    addEventListener('mousemove', event => {
-      mouse.x = event.clientX;
-      mouse.y = event.clientY;
-    });
-    
-    addEventListener('resize', () => {
-      canvas.width = innerWidth;
-      canvas.height = innerHeight;
-    
-      init();
-    });
-    
-    
-    // Utility Functions
-    function randomIntFromRange(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
+if (innerWidth <= 768) {
+  (mouse.x = innerWidth / 3.5), (mouse.y = innerHeight / 3.2);
+}
+
+if (innerWidth <= 480) {
+  (mouse.x = innerWidth / 2), (mouse.y = innerHeight / 1.65);
+}
+
+// Variables
+
+//color of the circle
+const colors = ["grey", "266da7", "slategrey"];
+
+// Event Listeners
+addEventListener("mousemove", event => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+// Utility Functions
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+// Objects
+function Particle(x, y, radius, color) {
+  let vardistance;
+
+  if (innerWidth <= 480) {
+    vardistance = randomIntFromRange(innerHeight / 12, innerHeight / 5);
+  } else if (innerWidth <= 768) {
+    vardistance = randomIntFromRange(innerHeight / 10, innerHeight / 6);
+  } else {
+    vardistance = randomIntFromRange(innerWidth / 16, innerWidth / 7.5);
+  }
+
+  const distance = vardistance;
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
+  this.radians = Math.random() * Math.PI * 2;
+  this.velocity = 0.045;
+  this.distanceFromCenter = {
+    x: distance,
+    y: distance
+  };
+
+  this.prevDistanceFromCenter = {
+    x: distance,
+    y: distance
+  };
+
+  this.lastMouse = { x: x, y: y };
+
+  this.update = () => {
+    const lastPoint = { x: this.x, y: this.y };
+    // Move points over time
+    this.radians += this.velocity;
+
+    // Drag effect
+    this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
+    this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+
+    //Circular Motion
+    this.distanceFromCenter.x =
+      this.prevDistanceFromCenter.x + Math.sin(this.radians);
+    this.distanceFromCenter.y =
+      this.prevDistanceFromCenter.x + Math.sin(this.radians);
+
+    this.x =
+      this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter.x;
+    this.y =
+      this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter.y;
+
+    this.draw(lastPoint);
+  };
+
+  this.draw = lastPoint => {
+    c.beginPath();
+    c.strokeStyle = this.color;
+    c.lineWidth = this.radius;
+    c.moveTo(lastPoint.x, lastPoint.y);
+    c.lineTo(this.x, this.y);
+    c.stroke();
+    c.closePath();
+  };
+}
+
+// Implementation
+let particles;
+function init() {
+  particles = [];
+
+  for (let i = 0; i < 60; i++) {
+    if (innerWidth <= 420 || innerHeight <= 420) {
+      // const radius = (Math.random() * 1) + 0.2;
+      // particles.push(new Particle(25, 25, radius, randomColor(colors)));
+    } else {
+      const radius = Math.random() * 1 + 1;
+      particles.push(new Particle(25, 25, radius, randomColor(colors)));
     }
+  }
+}
 
-    
-    function randomColor(colors) {
-      return colors[Math.floor(Math.random() * colors.length)];
-    }
-    
-    
-    // Objects
-    function Particle(x, y, radius, color) {
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = "rgba(246, 192, 20, 0.06)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-      let vardistance;
-      
-      if(innerWidth<=480){
-        vardistance = randomIntFromRange(innerHeight/12, innerHeight/5);
-      }
-      else if(innerWidth<=768){
-        vardistance = randomIntFromRange(innerHeight/10, innerHeight/6);
-      }
-      else{
-         vardistance = randomIntFromRange(innerWidth/16, innerWidth/7.5);
-      }
+  particles.forEach(particle => {
+    particle.update();
+  });
+}
 
-      const distance = vardistance;
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-      this.radians = Math.random() * Math.PI * 2;
-      this.velocity = 0.045;
-      this.distanceFromCenter = {
-        x: distance,
-        y: distance };
-    
-      this.prevDistanceFromCenter = {
-        x: distance,
-        y: distance };
-    
-      this.lastMouse = { x: x, y: y };
-    
-      this.update = () => {
-        const lastPoint = { x: this.x, y: this.y };
-        // Move points over time
-        this.radians += this.velocity;
-    
-        // Drag effect
-         this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
-         this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
-    
-         //Circular Motion
-          this.distanceFromCenter.x = this.prevDistanceFromCenter.x + Math.sin(this.radians) ;
-         this.distanceFromCenter.y = this.prevDistanceFromCenter.x + Math.sin(this.radians) ;
-    
-        this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter.x;
-        this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter.y;
-    
-        this.draw(lastPoint);
-      };
-    
-      this.draw = lastPoint => {
-        c.beginPath();
-        c.strokeStyle = this.color;
-        c.lineWidth = this.radius;
-        c.moveTo(lastPoint.x, lastPoint.y);
-        c.lineTo(this.x, this.y);
-        c.stroke();
-        c.closePath();
-      };
-    }
-    
-    
-    // Implementation
-    let particles;
-    function init() {
-      particles = [];
-    
-      for (let i = 0; i < 60; i++) {
-        if(innerWidth<=420 || innerHeight <=420){
-          // const radius = (Math.random() * 1) + 0.2;
-          // particles.push(new Particle(25, 25, radius, randomColor(colors)));
-        }
-        
-        else{
-          const radius = (Math.random() * 1) + 1;
-          particles.push(new Particle(25, 25, radius, randomColor(colors)));
-        }
-       
-      }
-    }
-    
-    // Animation Loop
-    function animate() {
-      requestAnimationFrame(animate);
-      c.fillStyle = 'rgba(246, 192, 20, 0.06)'; 
-      c.fillRect(0, 0, canvas.width, canvas.height);
-    
-      particles.forEach(particle => {
-        particle.update();
-      });
-    }
-    
-    init();
-    animate();
-  
+init();
+animate();
--- js/main.js
+++ js/main.js
@@ -1,100 +1,104 @@
-$(function($)
-{
-  "use strict"
+$(function($) {
+  "use strict";
 
-  // Preloader 
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
+  // Preloader
+  $(window).on("load", function() {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function() {
+          $(this).remove();
+        });
     }
   });
 
-//---------------HEADER-------------//
+  //---------------HEADER-------------//
 
-//for sticking the header    
-$(window).on('scroll',function(){
-
-    if($(this).scrollTop() > 80) {
-       $('.navbar').addClass('navbar-on-scroll');
-    }  
-    else{
-      $('.navbar').removeClass('navbar-on-scroll');
+  //for sticking the header
+  $(window).on("scroll", function() {
+    if ($(this).scrollTop() > 80) {
+      $(".navbar").addClass("navbar-on-scroll");
+    } else {
+      $(".navbar").removeClass("navbar-on-scroll");
     }
-});
+  });
 
+  //toggle hamburger on toggle
+  let hamburger = document.querySelector(".hamburger");
+  let navLinks = document.querySelectorAll(".navlink");
+  let links = document.querySelectorAll(".navlink a");
+  let menu = document.querySelector(".menu");
 
-//toggle hamburger on toggle
-let hamburger=document.querySelector('.hamburger');
-let navLinks=document.querySelectorAll('.navlink');
-let links=document.querySelectorAll('.navlink a');
-let menu=document.querySelector('.menu');
+  if (innerWidth <= 768) {
+    $(links).on("click", function() {
+      $(menu).toggleClass("menu-opened");
+      $(hamburger).toggleClass("open");
+      $(navLinks).toggleClass("fade");
+    });
+  }
 
-if(innerWidth <= 768){
-  $(links).on('click',function(){
+  $(hamburger).on("click", function() {
     $(menu).toggleClass("menu-opened");
-    $(hamburger).toggleClass("open");
-    $(navLinks).toggleClass("fade");
+    $(this).toggleClass("open");
+    //$(navLinks).toggleClass("fade");
+    // console.log("menu-opened");
   });
-  
-}
 
-$(hamburger).on('click',function(){
-   $(menu).toggleClass("menu-opened");
-   $(this).toggleClass("open");
-  //$(navLinks).toggleClass("fade");
-  // console.log("menu-opened");
- });
+  //initialize wow.js
 
-
- //initialize wow.js
-
- new WOW().init();
-
+  new WOW().init();
 
   // Smooth scroll for the navigation and links with .scrollto classes
-  $('.navlink a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $(".navlink a, .scrollto").on("click", function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
       if (target.length) {
         var top_space = 0;
 
-        if ($('.navbar').length) {
-          top_space = $('.navbar').outerHeight();
+        if ($(".navbar").length) {
+          top_space = $(".navbar").outerHeight();
 
-          if (! $('.navbar').hasClass('.navbar-on-scroll')) {
+          if (!$(".navbar").hasClass(".navbar-on-scroll")) {
             top_space = top_space - 20;
           }
         }
 
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1200, 'easeInOutQuint');
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1200,
+          "easeInOutQuint"
+        );
 
         // return false;
-       }
+      }
     }
   });
 
   // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.menu');
-  var main_nav_height = $('.navbar').outerHeight();
+  var nav_sections = $("section");
+  var main_nav = $(".menu");
+  var main_nav_height = $(".navbar").outerHeight();
 
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function() {
     var cur_pos = $(this).scrollTop();
-  
+
     nav_sections.each(function() {
       var top = $(this).offset().top - main_nav_height,
-          bottom = top + $(this).outerHeight();
-  
+        bottom = top + $(this).outerHeight();
+
       if (cur_pos >= top && cur_pos <= bottom) {
-        main_nav.find('li').removeClass('highlight');
-        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('highlight');
+        main_nav.find("li").removeClass("highlight");
+        main_nav
+          .find('a[href="#' + $(this).attr("id") + '"]')
+          .parent("li")
+          .addClass("highlight");
       }
     });
   });
-
 })(jQuery);
- 
